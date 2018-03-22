f4ec94edf16dea213a71765671d5e65c5de69beb
anoperson <nguyenhuuthien88bk@gmail.com>
2018-03-21 22:47:30 -0400

    adding Tfilm (film + nmn)

--- vr/models/tfilmed_net.py
+++ vr/models/tfilmed_net.py
@@ -78,20 +78,33 @@ class TFiLMedNet(nn.Module):
     # Initialize helper variables
     self.stem_use_coords = (stem_stride == 1) and (self.use_coords_freq > 0)
     self.condition_pattern = condition_pattern
+    self.first_condition_pattern = []
     if len(condition_pattern) == 0:
+      self.first_condition_pattern = [self.condition_method != 'concat']*2
       self.condition_pattern = []
-      
       for i in range(self.max_program_tree_depth):
         idepth = []
         for j in range(self.max_program_module_arity):
           ijarity = [[self.condition_method != 'concat']*2] * self.module_num_layers
           idepth.append(ijarity)
         self.condition_pattern.append(idepth)
-
     else:
+      ijc = 0
+      if len(self.condition_pattern) > ijc: val1 = self.condition_pattern[ijc]
+      else: val1 = self.condition_pattern[-1]
+      ijc += 1
+      if len(self.condition_pattern) > ijc: val2 = self.condition_pattern[ijc]
+      else: val2 = self.condition_pattern[-1]
+      self.first_condition_pattern = [val1 > 0, val2 > 0]
       for i in range(self.max_program_tree_depth):
         for j in range(self.max_program_module_arity):
-          self.condition_pattern[i][j] = [k > 0 for k in self.condition_pattern[i][j]]
+          ijc += 1
+          if len(self.condition_pattern) > ijc: val1 = self.condition_pattern[ijc]
+          else: val1 = self.condition_pattern[-1]
+          ijc += 1
+          if len(self.condition_pattern) > ijc: val2 = self.condition_pattern[ijc]
+          else: val2 = self.condition_pattern[-1]
+          self.condition_pattern[i][j] = [val1 > 0, val2 > 0]
     self.extra_channel_freq = self.use_coords_freq
     #self.block = FiLMedResBlock
     self.num_cond_maps = 2 * self.module_dim if self.condition_method == 'concat' else 0
@@ -121,7 +134,7 @@ class TFiLMedNet(nn.Module):
     #for fn_num in range(self.num_modules):
     
     mod = TfilmedResBlock(module_dim, with_residual=module_residual, with_batchnorm=module_batchnorm,
-                       with_cond=[True, True],
+                       with_cond=self.first_condition_pattern,
                        dropout=module_dropout,
                        num_extra_channels=self.num_extra_channels,
                        extra_channel_freq=self.extra_channel_freq,

12ffaa4a234871b52b9b33abb1f3fa138592e367
anoperson <nguyenhuuthien88bk@gmail.com>
2018-03-27 15:06:04 -0400

    removed rtfilm first

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -29,7 +29,7 @@ import vr.utils as utils
 import vr.preprocess
 from vr.data import ClevrDataset, ClevrDataLoader
 from vr.models import ModuleNet, Seq2Seq, LstmModel, CnnLstmModel, CnnLstmSaModel
-from vr.models import FiLMedNet, TFiLMedNet, RTFiLMedNet
+from vr.models import FiLMedNet, TFiLMedNet #, RTFiLMedNet
 from vr.models import FiLMGen
 
 from vr.treeGenerator import TreeGenerator

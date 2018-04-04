0f629a58bb87f53327f615ca93582d8b73e031e3
Michael Noukhovitch <mnoukhov@gmail.com>
2018-04-04 11:36:31 -0400

    fixed and tested

--- scripts/preprocess_shapes.py
+++ scripts/preprocess_shapes.py
@@ -22,7 +22,7 @@ def create_vocab(questions):
       if not w in question_vocab:
         question_vocab[w] = len(question_vocab)
 
-    return question_vocab
+  return question_vocab
 
 
 def vocab_to_module_text(program_vocab):
@@ -33,26 +33,28 @@ def vocab_to_module_text(program_vocab):
   for token in program_vocab:
     if token.startswith('_'):
       if '[' not in token:
-        # _And
-        module = token[1:]
+        # e.g. _And
+        module = token[1:].lower()
         text = None
       else:
-        # _Find[blue]
+        # e.g. _Find[blue]
         module, text = token.split('[')
         module = module[1:].lower()
-        text = text[:-1]
-        program_token_to_module_text[token] = (module, text)
-        text_tokens.add(text)
-        module_tokens.add(module)
+        text = text[:-1].lower()
+      text_tokens.add(text)
+      module_tokens.add(module)
+      program_token_to_module_text[token] = (module, text)
+    else:
+      program_token_to_module_text[token] = (None, None)
 
-    text_token_to_idx = {text: idx
-                         for idx, text in enumerate(text_tokens)}
-    module_token_to_idx = {module: idx
-                           for idx, module in enumerate(module_tokens)}
+  text_token_to_idx = {text: idx
+                        for idx, text in enumerate(text_tokens)}
+  module_token_to_idx = {module: idx
+                          for idx, module in enumerate(module_tokens)}
 
-    return (programs_token_to_module_text,
-            module_token_to_idx,
-            text_token_to_idx)
+  return (program_token_to_module_text,
+          module_token_to_idx,
+          text_token_to_idx)
 
 
 # An adaptation of the original code for parsing SHAPES queries
@@ -66,7 +68,7 @@ def extract_parse(p):
   elif isinstance(p, float):
     return str(p).lower()
 
-    return tuple(extract_parse(q) for q in p)
+  return tuple(extract_parse(q) for q in p)
 
 
 def parse_tree(p):
@@ -80,98 +82,102 @@ def layout_from_parsing(parse):
     return ("_Find[{}]".format(parse),)
   head = parse[0]
 
-    if len(parse) > 2:  # fuse multiple tokens with "_And"
-      assert(len(parse)) == 3
-      below = ("_And", layout_from_parsing(parse[1]),
-               layout_from_parsing(parse[2]))
-    else:
-      below = layout_from_parsing(parse[1])
+  if len(parse) > 2:  # fuse multiple tokens with "_And"
+    assert(len(parse)) == 3
+    below = ("_And", layout_from_parsing(parse[1]),
+              layout_from_parsing(parse[2]))
+  else:
+    below = layout_from_parsing(parse[1])
 
-    if head == "is":
-      module = "_Answer"
-    elif head in ["above", "below", "left_of", "right_of"]:
-      module = "_Transform[{}]".format(head)
+  if head == "is":
+    module = "_Answer"
+  elif head in ["above", "below", "left_of", "right_of"]:
+    module = "_Transform[{}]".format(head)
 
-    return (module, below)
+  return (module, below)
 
 
 def flatten_layout(module_layout):
-  # Postorder traversal to generate Reverse Polish Notation (RPN)
+  """Postorder traversal to generate Reverse Polish Notation (RPN)"""
   if isinstance(module_layout, str):
     return [module_layout]
 
-    PN = []
-    module = module_layout[0]
-    PN += [module]
-    for m in module_layout[1:]:
-      PN += flatten_layout(m)
+  PN = []
+  module = module_layout[0]
+  PN += [module]
+  for m in module_layout[1:]:
+    PN += flatten_layout(m)
 
-    return PN
+  return PN
 
 
-def main(shapes_data, size, programs):
+def main(shapes_data, size, program_type):
   parts = ['train', 'val', 'test']
   part_prefixes = ['train.' + size, 'val', 'test']
 
-    for part, prefix in zip(parts, part_prefixes):
-      image_path = "{}/shapes_dataset/{}.input.npy".format(shapes_data, prefix)
-      images = numpy.load(image_path)
+  for part, prefix in zip(parts, part_prefixes):
+    image_path = "{}/{}.input.npy".format(shapes_data, prefix)
+    images = numpy.load(image_path)
 
-        questions_path = "{}/shapes_dataset/{}.query_str.txt".format(shapes_data, prefix)
-        with open(questions_path) as src:
-          questions = [str_.split() for str_ in src]
-          max_question_len = max([len(q) for q in questions])
-          if part == 'train':
-            question_vocab = create_vocab(questions)
-            questions = [[question_vocab[w] for w in q] for q in questions]
-            questions_arr = numpy.zeros((len(questions), max_question_len), dtype='int64')
-            for row, q in zip(questions_arr, questions):
-              row[:][:len(q)] = q
+    questions_path = "{}/{}.query_str.txt".format(shapes_data, prefix)
+    with open(questions_path) as src:
+      questions = [str_.split() for str_ in src]
+    max_question_len = max([len(q) for q in questions])
+    if part == 'train':
+      question_vocab = create_vocab(questions)
+    questions = [[question_vocab[w] for w in q] for q in questions]
+    questions_arr = numpy.zeros((len(questions), max_question_len), dtype='int64')
+    for row, q in zip(questions_arr, questions):
+      row[:][:len(q)] = q
 
-        programs_path = "{}/shapes_dataset/{}.query".format(shapes_data, prefix)
-        programs = []
-        with open(programs_path) as src:
-          for line in src:
-            program = flatten_layout(layout_from_parsing(parse_tree(line)))
-            buffer_ = []
-            for token in program:
-              buffer_.append(token)
-              if token.startswith('_Find'):
-                buffer_.append('scene')
-                program = ['<START>'] + buffer_ + ['<END>']
-                programs.append(program)
-                max_program_len = max([len(p) for p in programs])
-                if part == 'train':
-                  program_vocab = create_vocab(programs)
-                  programs = [[program_vocab[w] for w in p] for p in programs]
-                  programs_arr = numpy.zeros((len(programs), max_program_len), dtype='int64')
-                  for row, p in zip(programs_arr, programs):
-                    row[:][:len(p)] = p
+    programs_path = "{}/{}.query".format(shapes_data, prefix)
+    programs = []
+    with open(programs_path) as src:
+      for line in src:
+        program = flatten_layout(layout_from_parsing(parse_tree(line)))
+        buffer_ = []
+        for token in program:
+          buffer_.append(token)
+          if token.startswith('_Find'):
+            buffer_.append('scene')
+        program = ['<START>'] + buffer_ + ['<END>']
+        programs.append(program)
+    max_program_len = max([len(p) for p in programs])
+    if part == 'train':
+      program_vocab = create_vocab(programs)
+    programs = [[program_vocab[w] for w in p] for p in programs]
+    programs_arr = numpy.zeros((len(programs), max_program_len), dtype='int64')
+    for row, p in zip(programs_arr, programs):
+      row[:][:len(p)] = p
 
-        answers_path = "{}/shapes_dataset/{}.output".format(shapes_data, prefix)
-        with open(answers_path) as src:
-          answers = [1 if w.strip() == 'true' else 0 for w in src]
+    answers_path = "{}/{}.output".format(shapes_data, prefix)
+    with open(answers_path) as src:
+      answers = [1 if w.strip() == 'true' else 0 for w in src]
 
-        with h5py.File(part + '_features.h5', 'w') as dst:
-          features = images.transpose(0, 3, 1, 2) / 255.0
-          features_dataset = dst.create_dataset(
-            'features', (features.shape), dtype=numpy.float32)
-          features_dataset[:] = features
-
-        with h5py.File(part + '_questions.h5', 'w') as dst:
-          questions_dataset = dst.create_dataset(
-            'questions', (len(questions), max_question_len), dtype=numpy.int64)
-          questions_dataset[:] = questions_arr
-          if programs == 'chain':
-            programs_dataset = dst.create_dataset(
-              'programs', (len(programs), max_program_len), dtype=numpy.int64)
-            programs_dataset[:] = programs_arr
-            answers_dataset = dst.create_dataset(
-              'answers', (len(questions),), dtype=numpy.int64)
-            answers_dataset[:] = answers
-            image_idxs_dataset = dst.create_dataset(
-              'image_idxs', (len(questions),), dtype=numpy.int64)
-            image_idxs_dataset[:] = range(len(questions))
+    with h5py.File(part + '_features.h5', 'w') as dst:
+      features = images.transpose(0, 3, 1, 2) / 255.0
+      features_dataset = dst.create_dataset('features',
+                                            (features.shape),
+                                            dtype=numpy.float32)
+      features_dataset[:] = features
+    with h5py.File(part + '_questions.h5', 'w') as dst:
+      questions_dataset = dst.create_dataset('questions',
+                                             (len(questions), max_question_len),
+                                             dtype=numpy.int64)
+      questions_dataset[:] = questions_arr
+      if program_type == 'chain':
+        programs_dataset = dst.create_dataset('programs',
+                                              (len(programs), max_program_len),
+                                              dtype=numpy.int64)
+        programs_dataset[:] = programs_arr
+      answers_dataset = dst.create_dataset('answers',
+                                            (len(questions),),
+                                            dtype=numpy.int64)
+      answers_dataset[:] = answers
+      image_idxs_dataset = dst.create_dataset('image_idxs',
+                                              (len(questions),),
+                                              dtype=numpy.int64)
+      image_idxs_dataset[:] = range(len(questions))
 
 
     (program_token_to_module_text,
@@ -185,8 +191,7 @@ def main(shapes_data, size, programs):
                  'answer_token_to_idx': {'false': 0, 'true': 1},
                  'program_token_to_module_text': program_token_to_module_text,
                  'module_token_to_idx': module_token_to_idx,
-                 'text_token_to_idx': text_token_to_idx,
-                 },
+                 'text_token_to_idx': text_token_to_idx},
                 dst)
 
 

d02f11b7b070d8c600d7f89705fc1d4baa32a35f
rizar <dimabv@tut.by>
2018-04-19 12:53:06 -0400

    relations

--- scripts/generate_flatqa.py
+++ scripts/generate_flatqa.py
@@ -32,6 +32,7 @@ from PIL import Image, ImageDraw
 
 
 logger = logging.getLogger(__name__)
+RELATIONS = ['left_of', 'right_of', 'above', 'below']
 COLORS = ['red', 'green', 'blue', 'yellow', 'cyan',
           'purple', 'brown', 'gray']
 SHAPES = ['square', 'triangle',  'cross', 'bar',
@@ -53,6 +54,17 @@ class Object(object):
     min_dist = (self.rotated_size + other.rotated_size) // 2 + 1
     return (abs(self.pos[0] - other.pos[0]) < min_dist and
             abs(self.pos[1] - other.pos[1]) < min_dist)
+
+  def relate(self, rel, other):
+    if rel == 'left_of':
+      return self.pos[0] < other.pos[0]
+    if rel == 'right_of':
+      return self.pos[0] > other.pos[0]
+    if rel == 'above':
+      return self.pos[1] > other.pos[1]
+    if rel == 'below':
+      return self.pos[1] < other.pos[1]
+    raise ValueError(rel)
 
 
 class CustomJSONEncoder(json.JSONEncoder):
@@ -130,8 +142,12 @@ def get_random_spot(rng, objects):
     obj.pos = (x, y)
 
     # make sure there is no overlap between bounding squares
-    if not any([obj.overlap(other) for other in objects]):
-      return obj
+    if (any([abs(obj.pos[0] - other.pos[0]) < 5 for other in objects]) or
+        any([abs(obj.pos[1] - other.pos[1]) < 5 for other in objects])):
+      continue
+    if any([obj.overlap(other) for other in objects]):
+      continue
+    return obj
   else:
     return None
 
@@ -142,6 +158,10 @@ def shape_module(shape):
 
 def color_module(color):
   return "Color[{}]".format(color)
+
+
+def relation_module(relation):
+  return "Relate[{}]".format(relation)
 
 
 def rejection_sample(rng, shapes, colors, restricted=[]):
@@ -181,17 +201,50 @@ def generate_dataset(prefix, num_examples, seed, sample_shape_color, save_vocab=
   shapes = SHAPES[:args.num_shapes]
   colors = COLORS[:args.num_colors]
 
-  max_question_len = 5
-  max_program_len = 7
+  if args.level == 'shapecolor':
+    max_question_len = 5
+    max_program_len = 7
+  elif args.level == 'relations':
+    max_question_len = 8
+    max_program_len = 12
 
   question_words = (['<NULL>', '<START>', '<END>', 'is', 'there', 'a']
-                    + colors + shapes)
+                    + colors + shapes + RELATIONS)
   question_vocab = {word: i for i, word in enumerate(question_words)}
 
   program_words = (['<NULL>', '<START>', '<END>', 'scene', 'And']
                    + [color_module(color) for color in colors]
-                   + [shape_module(shape) for shape in shapes])
+                   + [shape_module(shape) for shape in shapes]
+                   + [relation_module(rel) for rel in RELATIONS])
   program_vocab = {word: i for i, word in enumerate(program_words)}
+
+  answer_token_to_idx = {word: idx for idx, word in
+                         enumerate(['false', 'true'])}
+  module_token_to_idx = {word: idx for idx, word in
+                         enumerate(['Color', 'Shape', 'And', 'Relate'])}
+  program_token_to_module_text = {}
+  for color in colors:
+    program_token_to_module_text[color_module(color)] = ['Color', color]
+  for shape in shapes:
+    program_token_to_module_text[shape_module(shape)] = ['Shape', shape]
+  for rel in RELATIONS:
+    program_token_to_module_text[relation_module(rel)] = ['Relate', rel]
+  program_token_to_module_text['And'] = ('And', 'null')
+  for module in ['<START>', '<END>', '<NULL>']:
+    program_token_to_module_text[module] = ('null', 'null')
+
+  text_token_to_idx = {}
+  for idx, word in enumerate(['null'] + colors + shapes + RELATIONS):
+    text_token_to_idx[word] = idx
+
+  def arity(token):
+    if token == 'And' or token.startswith('Relate'):
+      return 2
+    elif token == 'scene':
+      return 0
+    else:
+      return 1
+  program_token_arity = {name: arity(name) for name in program_vocab},
 
   rng = numpy.random.RandomState(seed)
   scenes = []
@@ -215,36 +268,88 @@ def generate_dataset(prefix, num_examples, seed, sample_shape_color, save_vocab=
       'image_idxs', (num_examples,), dtype=numpy.int64)
 
     rng = numpy.random.RandomState(seed)
-    for i in range(num_examples):
+    i = 0
+    while i < num_examples:
       if i and i % 1000 == 0:
         print(i)
 
-      # choose the question
-      shape, color = sample_shape_color(purpose='ask')
-
       answer = i % 2
-      if answer:
-        # first, sample the witness object and place it
-        obj = get_random_spot(rng, [])
-        obj.shape = shape
-        obj.color = color
-        # complete the scene by sampling a random object
-        scene = generate_scene(rng, sample_shape_color, objects=[obj])
-      else:
-        # sample a scene and check if the answer is no
-        # if failed 10 times, try another question
-        for attempt in range(11):
-          scene = generate_scene(rng, sample_shape_color)
-          found = any((shape, color) == (obj.shape, obj.color)
-                      for obj in scene)
-          if not found:
-            break
-        if attempt == 10:
-          continue
 
-      question = ["is", "there", "a"] + [color, shape]
-      program = ['<START>', 'And', shape_module(shape), 'scene',
-                 color_module(color), 'scene', '<END>']
+      if args.level == 'shapecolor': # a shape-color question
+        shape, color = sample_shape_color(purpose='ask')
+        if answer:
+          # first, sample the witness object and place it
+          obj = get_random_spot(rng, [])
+          obj.shape = shape
+          obj.color = color
+          # complete the scene by sampling a random object
+          scene = generate_scene(rng, sample_shape_color, objects=[obj])
+        else:
+          # sample a scene and check if the answer is no
+          # if failed 10 times, try another question
+          scene = generate_scene(rng, sample_shape_color)
+          if any((shape, color) == (obj.shape, obj.color) for obj in scene):
+            continue
+        question = ["is", "there", "a"] + [color, shape]
+        program = ['<START>', 'And', shape_module(shape), 'scene',
+                  color_module(color), 'scene', '<END>']
+      elif args.level == 'relations':
+        if answer:
+          rel = rng.choice(RELATIONS)
+          # first, select two spots for which the relation holds
+          obj1 = get_random_spot(rng, [])
+          obj2 = get_random_spot(rng, [obj1])
+          if not obj2 or not obj1.relate(rel, obj2):
+            continue
+          # second, select shapes and colors
+          shape1, color1 = sample_shape_color(purpose='ask')
+          obj1.shape = shape1
+          obj1.color = color1
+          shape2, color2 = sample_shape_color(purpose='ask')
+          obj2.shape = shape2
+          obj2.color = color2
+          # lastly, fill the scene with other objects
+          scene = generate_scene(rng, sample_shape_color, objects=[obj1, obj2])
+        else:
+          # first generate a scene
+          scene = generate_scene(rng, sample_shape_color)
+
+          # Choose a question for which the answer is false. Note, that
+          # generating a completely random question might be a bad idea
+          # because it will be to easy to detect as false. Hence:
+          neg_question_type = rng.choice(['rel', 'lhs', 'rhs'])
+          if neg_question_type == 'rel':
+            obj1 = scene[rng.randint(len(scene))]
+            shape1, color1 = obj1.shape, obj1.color
+            obj2 = scene[rng.randint(len(scene))]
+            shape2, color2 = obj2.shape, obj2.color
+            rel = rng.choice([rel for rel in RELATIONS if not obj1.relate(rel, obj2)])
+          elif neg_question_type == 'lhs':
+            rel = rng.choice(RELATIONS)
+            shape1, color1 = sample_shape_color(purpose='ask')
+            obj2 = scene[rng.randint(len(scene))]
+            shape2, color2 = obj2.shape, obj2.color
+          elif neg_question_type == 'rhs':
+            rel = rng.choice(RELATIONS)
+            obj1 = scene[rng.randint(len(scene))]
+            shape1, color1 = obj1.shape, obj1.color
+            shape2, color2 = sample_shape_color(purpose='ask')
+
+          # verify if the answer to the question is False
+          relation_holds = False
+          for obj1 in scene:
+            for obj2 in scene:
+              if (obj1 != obj2 and obj1.relate(rel, obj2)
+                  and obj1.shape == shape1 and obj1.color == color1
+                  and obj2.shape == shape2 and obj2.color == color2):
+                relation_holds = True
+          if relation_holds:
+            continue
+        question = ["is", "there", "a",
+                    color1, shape1, rel, color2, shape2]
+        program = ["<START>", relation_module(rel),
+                    "And", shape_module(shape1), "scene", color_module(color1), "scene",
+                    "And", shape_module(shape2), "scene", color_module(color2), "scene"]
 
       scenes.append(scene)
       buffer_ = io.BytesIO()
@@ -258,33 +363,14 @@ def generate_dataset(prefix, num_examples, seed, sample_shape_color, save_vocab=
       answers_dataset[i] = int(answer)
       image_idxs_dataset[i] = i
 
+      i += 1
+
   print("{} examples per second".format((time.time() - before) / num_examples))
 
   with open(prefix + '_scenes.json', 'w') as dst:
     json.dump(scenes, dst, indent=2, cls=CustomJSONEncoder)
 
-  answer_token_to_idx = {'false': 0, 'true': 1}
-  module_token_to_idx = {'Color': 0, 'Shape': 1, 'And': 2}
-  program_token_to_module_text = {}
-  text_token_to_idx = {}
-  for color in colors:
-    program_token_to_module_text[color_module(color)] = ['Color', color]
-  for shape in shapes:
-    program_token_to_module_text[shape_module(shape)] = ['Shape', shape]
-  program_token_to_module_text['And'] = ('And', 'null')
-  for module in ['<START>', '<END>', '<NULL>']:
-    program_token_to_module_text[module] = ('null', 'null')
-  for idx, word in enumerate(['null'] + colors + shapes):
-    text_token_to_idx[word] = idx
-
   if save_vocab:
-    def arity(token):
-      if token == 'And':
-        return 2
-      elif token == 'scene':
-        return 0
-      else:
-        return 1
     with open('vocab.json', 'w') as dst:
       json.dump({'question_token_to_idx': question_vocab,
                  'program_token_to_idx': program_vocab,
@@ -298,7 +384,9 @@ def generate_dataset(prefix, num_examples, seed, sample_shape_color, save_vocab=
 
 
 def main():
-  train_object_allowed = val_object_allowed = test_object_allowed = lambda _1, _2: True
+  def default_sample(rng, shapes, colors, purpose):
+    return rejection_sample(rng, shapes, colors)
+  train_sample = val_sample = test_sample = default_sample
 
   if args.split == 'CoGenT':
     set1 = ['gray', 'blue', 'brown', 'yellow']
@@ -357,10 +445,11 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--train', type=int, default=1000,
                       help="Size of the training set")
-  parser.add_argument('--val', type=int, default=100,
+  parser.add_argument('--val', type=int, default=1000,
                       help="Size of the development set")
-  parser.add_argument('--test', type=int, default=100,
+  parser.add_argument('--test', type=int, default=1000,
                       help="Size of the test set")
+  parser.add_argument('--level', type=str, choices=('shapecolor', 'relations'), default='shapecolor')
   parser.add_argument('--num-shapes', type=int, default=len(SHAPES))
   parser.add_argument('--num-colors', type=int, default=len(COLORS))
   parser.add_argument('--num-objects', type=int, default=5)

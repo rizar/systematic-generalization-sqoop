802064147437c4afca77ac123e21e46072e6b9d5
Thien Nguyen <nguthien@leto05.iro.umontreal.ca>
2018-04-24 16:57:46 -0400

    tmac minor fix

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -881,7 +881,7 @@ def get_execution_engine(args):
                 #'memory_dropout': args.mac_memory_dropout,
                 'read_dropout': args.mac_read_dropout,
                 'use_prior_control_in_control_unit': args.mac_use_prior_control_in_control_unit == 1,
-                'sharing_params_patterns': parse_int_list(args.mac_sharing_params_patterns),
+                'sharing_params_patterns': parse_int_list(args.tmac_sharing_params_patterns),
                 #'use_self_attention': args.mac_use_self_attention == 1,
                 #'use_memory_gate': args.mac_use_memory_gate == 1,
                 #'use_memory_lstm': args.mac_use_memory_lstm == 1,

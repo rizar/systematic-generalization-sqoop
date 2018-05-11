c8acc2b56d9814c5e80575476ea19f615edf4891
Thien Nguyen <nguthien@leto05.iro.umontreal.ca>
2018-05-11 13:33:45 -0400

    script to run nmnfilm2

---
+++ scripts/train/nmnfilm2_flatqa.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+THIS_SCRIPT_DIR=`dirname $0`
+
+python $THIS_SCRIPT_DIR/../train_model.py \
+  --model_type NMNFilm2 \
+  --num_iterations 50000 \
+  --feature_dim=3,64,64 \
+  --checkpoint_every 100 \
+  --record_loss_every 10 \
+  --num_val_samples 1000 \
+  --optimizer Adam \
+  --learning_rate 1e-4 \
+  --batch_size 64 \
+  --use_coords 1 \
+  --nmnfilm2_sharing_params_patterns 0,0 \
+  --module_stem_batchnorm 1 \
+  --module_stem_num_layers 4 \
+  --module_stem_subsample_layers 1\
+  --module_batchnorm 1 \
+  --classifier_batchnorm 1 \
+  --bidirectional 0 \
+  --decoder_type linear \
+  --encoder_type gru \
+  --weight_decay 1e-5 \
+  --rnn_num_layers 1 \
+  --rnn_wordvec_dim 200 \
+  --rnn_hidden_dim 1024 `#was 4096 in original FiLM` \
+  --rnn_output_batchnorm 0 \
+  --classifier_downsample maxpoolfull \
+  --classifier_proj_dim 512 \
+  --classifier_fc_dims 1024 \
+  --module_input_proj 1 \
+  --module_residual 1 \
+  --module_dim 64 `#was 128 in original FiLM`\
+  --module_dropout 0e-2 \
+  --module_stem_kernel_size 3 \
+  --module_kernel_size 3 \
+  --module_batchnorm_affine 0 \
+  --module_num_layers 1 \
+  --condition_pattern 1,1 \
+  --gamma_option linear \
+  --gamma_baseline 1 \
+  --use_gamma 1 \
+  --use_beta 1 \
+  --use_local_copies 0 \
+  --condition_method bn-film \
+  --program_generator_parameter_efficient 1 $@

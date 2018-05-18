b156de576ac4975100b0bbad69e6b0a410678f7d
rizar <dimabv@tut.by>
2018-05-18 17:50:01 -0400

    update film flatqa script

--- scripts/train/film_flatqa.sh
+++ scripts/train/film_flatqa.sh
@@ -14,8 +14,8 @@ python $THIS_SCRIPT_DIR/../train_model.py \
   --batch_size 64 \
   --use_coords 1 \
   --module_stem_batchnorm 1 \
-  --module_stem_num_layers 4 \
-  --module_stem_subsample_layers 1\
+  --module_stem_num_layers 6 \
+  --module_stem_subsample_layers 1,3\
   --module_batchnorm 1 \
   --classifier_batchnorm 1 \
   --bidirectional 0 \

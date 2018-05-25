e5dc40145d6ce51e4ed74ee3d5054aa1d8fad79e
Dzmitry Bahdanau <dimabgv@gmail.com>
2018-05-25 10:55:12 -0700

    update ee_film hyperparameters

--- scripts/train/ee_film_flatqa.sh
+++ scripts/train/ee_film_flatqa.sh
@@ -12,14 +12,15 @@ python $THIS_SCRIPT_DIR/../train_model.py \
   --record_loss_every 10 \
   --num_val_samples 1000 \
   --optimizer Adam \
-  --learning_rate 1e-4 \
+  --learning_rate 3e-4 \
   --use_coords 1 \
   --module_stem_batchnorm 1 \
   --module_stem_num_layers 6 \
   --module_stem_subsample_layers 1,3\
   --module_intermediate_batchnorm 0 \
   --module_batchnorm 0 \
-  --module_dim 64 \
+  --module_dim 128 \
+  --module_input_proj 0 \
   --classifier_batchnorm 1 \
   --weight_decay 1e-5 \
   --classifier_downsample maxpoolfull \

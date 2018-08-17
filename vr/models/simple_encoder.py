b69572fb77892aa16df462a8daf61f1a8025bc1a
MurtyShikhar <shikhar.murty@gmail.com>
2018-08-17 15:08:48 -0400

    tab fix

--- vr/models/simple_encoder.py
+++ vr/models/simple_encoder.py
@@ -12,25 +12,25 @@ from vr.models.layers import init_modules
 from torch.nn.init import uniform, xavier_uniform, constant
 
 class SimpleEncoderBinary(nn.Module):
-	'''
-	SimpleEncoder takes a question extracts the important info (for binary relation VQA: s1, r, s2)
-	embeds them into a learnt vector space and then concatenates them
-	'''
+  '''
+  SimpleEncoder takes a question extracts the important info (for binary relation VQA: s1, r, s2)
+  embeds them into a learnt vector space and then concatenates them
+  '''
 
-	def __init__(self, vocab, embedding_dim_in, hidden_dim, embedding_dim_out):
-		super(SimpleEncoder, self).__init__() 
-		self.encoder   = nn.Embedding(vocab, embedding_dim_in)
+  def __init__(self, vocab, embedding_dim_in, hidden_dim, embedding_dim_out):
+    super(SimpleEncoder, self).__init__() 
+    self.encoder = nn.Embedding(vocab, embedding_dim_in)
  
-		self.feed_forward_1 = nn.Linear(3*embedding_dim_in, hidden_dim)
-		self.feed_forward_2 = nn.Linear(hidden_dim, embedding_dim_out)
+    self.feed_forward_1 = nn.Linear(3*embedding_dim_in, hidden_dim)
+    self.feed_forward_2 = nn.Linear(hidden_dim, embedding_dim_out)
 
-		self.tanh = nn.Tanh()
+    self.tanh = nn.Tanh()
 
-	def forward(self, x): 
-		s1_embed = self.encoder(x[:, 4]) # B x D
-		s2_embed = self.encoder(x[:, 6]) # B x D 
-		relation_embed = self.encoder(x[:, 5]) # B x D
+  def forward(self, x): 
+    s1_embed = self.encoder(x[:, 4]) # B x D
+    s2_embed = self.encoder(x[:, 6]) # B x D 
+    relation_embed = self.encoder(x[:, 5]) # B x D
 
-		return self.feed_forward_2(self.tanh(self.feed_forward_1(torch.cat([s1_embed, s2_embed, relation_embed], dim = -1))))
+    return self.feed_forward_2(self.tanh(self.feed_forward_1(torch.cat([s1_embed, s2_embed, relation_embed], dim = -1))))
 
 

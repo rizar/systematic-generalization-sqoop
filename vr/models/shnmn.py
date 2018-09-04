c9fd66a6738b67b1c48170dcf35174ae236f75af
MurtyShikhar <shikhar.murty@gmail.com>
2018-09-04 13:52:14 -0400

    first model done.

--- vr/models/shnmn.py
+++ vr/models/shnmn.py
@@ -17,6 +17,10 @@ from vr.models.tfilmed_net import ConcatFiLMedResBlock
 from vr.models.filmed_net import FiLM, FiLMedResBlock, coord_map
 from functools import partial
 
+IN=16
+OUT=16
+H=3
+W=3
 
 def _softmax(vec):
     '''
@@ -25,6 +29,32 @@ def _softmax(vec):
     max_weight = torch.max(vec)
     unnormalized_weights =  torch.exp(vec-max_weight) 
     return unnormalized_weights / torch.sum(unnormalized_weights)
+
+
+class ConvFunc(nn.Module):
+    def __init__(self, dim, kernel_size):
+        super(ConvFunc, self).__init__()
+        self.proj = nn.Conv2d(2*dim, dim, kernel_size=1, padding = 0)
+        self.dim = dim
+        self.kernel_size = kernel_size
+
+    def forward(question_rep, lhs_rep, rhs_rep):
+        cnn_weight_dim = self.dim*self.dim*self.kernel_size*self.kernel_size
+        cnn_bias_dim = self.dim
+        if question_rep.size(1) != cnn_weight_dim + cnn_bias_dim: raise ValueError
+        cnn_weight = question_rep[: cnn_weight_dim]
+        cnn_bias = question_rep[cnn_weight_dim : ]
+
+        cnn_out_total = []
+        bs = question_rep.size(0)
+        cnn_inp = self.proj(torch.cat([x,y], 1))
+
+        for i in range(bs):
+            cnn_weight_curr = cnn_weight[i].view(self.dim, self.dim, self.kernel_size, self.kernel_size)
+            cnn_bias_curr   = cnn_weight[i].view(self.dim)
+            cnn_out_total.append(F.conv2d(cnn_inp[[i]], cnn_weight_curr, cnn_bias_curr))
+
+        return torch.cat(cnn_out_total)
 
 class SHNMN(nn.Module):
     def __init__(self,feature_dim, stem_dim, module_dim, stem_num_layers, 
@@ -59,16 +89,15 @@ class SHNMN(nn.Module):
                           with_batchnorm=classifier_batchnorm
                           dropout=classifier_dropout) 
 
-        self.func = None # Biggest TODO
+        self.func = ConvFunc(module_dim, module_kernel_size)
     
     def forward(self, image, question):
-        h_prev = self.stem(image).unsqueeze(1) # B x1 x H x W x C
+        h_prev = self.stem(image).unsqueeze(1) # B x1 x C x H x W
         for i in range(self.num_modules):
             question_rep = torch.sum( _softmax(self.alphas[i]).view(1,-1,1)*question, dim=1) #(B,D)
-            lhs_rep = torch.sum(_softmax(self.tau_0[i, :(i+1)]).view(1, (i+1), 1, 1, 1)*h_prev, dim=1) # B x H x W x C  
-            rhs_rep = torch.sum(_softmax(self.tau_0[i, :(i+1)]).view(1, (i+1), 1, 1, 1)*h_prev, dim=1) # B x H x W x C
-            # use a hypernet that takes the question and returns a 3 x 3 x 16 x 16 convolution filter bank? 
-            h_i = self.func(question_rep, lhs_rep, rhs_rep)
+            lhs_rep = torch.sum(_softmax(self.tau_0[i, :(i+1)]).view(1, (i+1), 1, 1, 1)*h_prev, dim=1) # B x C x H x W  
+            rhs_rep = torch.sum(_softmax(self.tau_1[i, :(i+1)]).view(1, (i+1), 1, 1, 1)*h_prev, dim=1) # B x C x H x W
+            h_i = self.func(question_rep, lhs_rep, rhs_rep) #
 
             h_prev = torch.stack([h_prev, h_i.unsqueeze(1)], dim = 1)
 

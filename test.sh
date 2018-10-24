ba308bd121de54d8f806b2d29e99d4aaf298ce61
rizar <dimabv@tut.by>
2018-10-24 13:27:45 -0400

    test more

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+set -e
+
 ARGS="--batch_size=2 --record_loss_every=1 --checkpoint_every=2 --num_iterations=5 --num_val_samples=4"
 export NMN=`pwd`
 
@@ -5,3 +7,7 @@ python scripts/dataset/generate_flatqa_letters_2.py --num-shapes=3 --num_repeats
 bash scripts/train/film_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=film_flatqa
 bash scripts/train/mac_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=mac_flatqa
 bash scripts/train/ee_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=ee_flatqa
+bash scripts/train/ee_new_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=ee_new_flatqa
+bash scripts/train/rel_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=rel_flatqa
+bash scripts/train/convlstm_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=convlstm_flatqa
+bash scripts/train/shnmn.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=shnmn_flatqa

49ab3cafd0cc9e6fc0a77e4a3ac4d3f29793409b
rizar <dimabv@tut.by>
2018-10-24 12:02:52 -0400

    speed up test

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
+ARGS="--batch_size=4 --record_loss_every=1 --checkpoint_every=3 --num_iterations=8 --num_val_samples=8"
+
 python scripts/dataset/generate_flatqa_letters_2.py --num-shapes=3 --num_repeats=100 --rhs_variety=1 --font=DejaVuSans.ttf
-bash scripts/train/film_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 --record_loss_every=1 --checkpoint_every=3 --num_iterations=8 --checkpoint_path=film_flatqa
-bash scripts/train/mac_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 --record_loss_every=1 --checkpoint_every=3 --num_iterations=8 --checkpoint_path=mac_flatqa
-bash scripts/train/ee_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 --record_loss_every=1 --checkpoint_every=3 --num_iterations=8 --checkpoint_path=ee_flatqa
+bash scripts/train/film_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=film_flatqa
+bash scripts/train/mac_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=mac_flatqa
+bash scripts/train/ee_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_100 $ARGS --checkpoint_path=ee_flatqa

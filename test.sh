17aae8cca6836d4150033354ef0d6bec5f0c37ee
rizar <dimabv@tut.by>
2018-10-24 11:48:50 -0400

    add checkpoint_path in the test

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-python scripts/dataset/generate_flatqa_letters_2.py  --num-shapes=3 --num_repeats=4 --rhs_variety=1 --font=DejaVuSans.ttf
-bash scripts/train/film_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_4 --record_loss_every=1 --checkpoint_every=3 --num_iterations=8
+python scripts/dataset/generate_flatqa_letters_2.py --num-shapes=3 --num_repeats=4 --rhs_variety=1 --font=DejaVuSans.ttf
+bash scripts/train/film_flatqa.sh --data_dir flatqa-letters-variety_1-repeats_4 --record_loss_every=1 --checkpoint_every=3 --num_iterations=8 --checkpoint_path=film_flatqa
